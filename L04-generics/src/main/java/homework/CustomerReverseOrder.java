18a08abfd24b91bf51e834a3025c8e8975f0eb78
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-03-23 11:54:52 +0300

    домашняя работа

--- L04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,14 +1,15 @@
 package homework;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
+import java.util.Stack;
+
 public class CustomerReverseOrder {
+    private Stack<Customer> stack = new Stack<>();
 
-    // todo: 2. надо реализовать методы этого класса
-    // надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-
-    public void add(Customer customer) {}
+    public void add(Customer customer) {
+        stack.push(customer);
+    }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return stack.pop();
     }
 }
