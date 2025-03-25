305d441d10c0c2dc07f4c5d5a3403aa3483f695a
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-03-25 20:31:20 +0300

    домашняя работа, исправления

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -1,18 +1,26 @@
 package homework;
 
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class CustomerService {
-    private TreeMap<Customer, String> customerData = new TreeMap<>();
+    private NavigableMap<Customer, String> customerData = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
-        return customerData.firstEntry();
+        Map.Entry<Customer, String> smallestEntry = customerData.firstEntry();
+        if (smallestEntry != null) {
+            return Map.entry(new Customer(smallestEntry.getKey()), smallestEntry.getValue());
+        }
+        return null;
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
         Map.Entry<Customer, String> nextEntry = customerData.higherEntry(customer);
-        return nextEntry != null ? nextEntry : null;
+        if (nextEntry != null) {
+            return Map.entry(new Customer(nextEntry.getKey()), nextEntry.getValue());
+        }
+        return null;
     }
 
     public void add(Customer customer, String data) {
