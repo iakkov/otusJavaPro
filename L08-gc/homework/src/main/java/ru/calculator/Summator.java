7e18c02e02081c4e6e3d3f5a84d1a88e6cd32705
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-03-31 23:47:43 +0300

    Исправленная домашняя работа

--- L08-gc/homework/src/main/java/ru/calculator/Summator.java
+++ L08-gc/homework/src/main/java/ru/calculator/Summator.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
     // !!! эта коллекция должна остаться. Заменять ее на счетчик нельзя.
     private final List<Data> listValues = new ArrayList<>();
     private final SecureRandom random = new SecureRandom();
@@ -27,29 +27,29 @@ public class Summator {
         prevPrevValue = prevValue;
         prevValue = data.getValue();
 
-        for (var idx = 0; idx < 3; idx++) {
+        for (int idx = 0; idx < 3; idx++) {
             someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
             someValue = Math.abs(someValue) + listValues.size();
         }
     }
 
-    public Integer getSum() {
+    public int getSum() {
         return sum;
     }
 
-    public Integer getPrevValue() {
+    public int getPrevValue() {
         return prevValue;
     }
 
-    public Integer getPrevPrevValue() {
+    public int getPrevPrevValue() {
         return prevPrevValue;
     }
 
-    public Integer getSumLastThreeValues() {
+    public int getSumLastThreeValues() {
         return sumLastThreeValues;
     }
 
-    public Integer getSomeValue() {
+    public int getSomeValue() {
         return someValue;
     }
 }
