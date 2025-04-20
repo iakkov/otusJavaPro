a3adefcda63636b7569f1be2a54b824cf349c407
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-04-20 13:16:54 +0300

    обработка исключений

--- L00-other/src/ru/project/web/HttpServer.java
+++ L00-other/src/ru/project/web/HttpServer.java
@@ -1,24 +1,54 @@
 package ru.project.web;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-        Socket socket = serverSocket.accept();
+    private int port;
+    private boolean shutdown = false;
 
-        byte[] buffer = new byte[4096];
-        while(true) {
-            int n = socket.getInputStream().read(buffer);
-            String message = new String(buffer, 0, n);
-            System.out.print(message);
-            if (message.equals("/shutdown")) {
-                break;
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public void await() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Сервер запущен на порту " + port);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!shutdown) {
+            try (Socket socket = serverSocket.accept();
+                 InputStream input = socket.getInputStream();
+                 OutputStream output = socket.getOutputStream()){
+                System.out.println("Получен входящий запрос");
+                byte[] buffer = new byte[4096];
+                while (true) {
+                    int n = socket.getInputStream().read(buffer);
+                    String message = new String(buffer, 0, n);
+                    System.out.print(message);
+                    if (message.equals("/shutdown")) {
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-            socket.close();
-            serverSocket.close();
+    }
+
+    public static void main(String[] args) {
+        HttpServer httpServer = new HttpServer(8189);
+        try {
+            httpServer.await();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+}
